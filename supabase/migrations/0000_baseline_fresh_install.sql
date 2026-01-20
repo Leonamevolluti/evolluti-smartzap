385cb02393831bfa78b4933e0648620017c1be37
thaleslaray <thales@laray.com.br>
2026-01-20 12:41:14 -0300

    refactor: remover criação e comentário do esquema público no script de instalação

--- supabase/migrations/0000_baseline_fresh_install.sql
+++ supabase/migrations/0000_baseline_fresh_install.sql
@@ -18,20 +18,6 @@ SET client_min_messages = warning;
 SET row_security = off;
 
 --
--- Name: public; Type: SCHEMA; Schema: -; Owner: -
---
-
-CREATE SCHEMA IF NOT EXISTS public;
-
-
---
--- Name: SCHEMA public; Type: COMMENT; Schema: -; Owner: -
---
-
-COMMENT ON SCHEMA public IS 'standard public schema';
-
-
---
 -- Name: get_dashboard_stats(); Type: FUNCTION; Schema: public; Owner: -
 --
 
